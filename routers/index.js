2f4e08ae9e361e665d160e2e4baee05bc8f778e9
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-30 09:12:12 -0300

    Criando rotas

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const TodoRouter = require('./TodoRouter');
 
-router.use('/', TodoRouter);
+router.use('/todo', TodoRouter);
 
 module.exports = router;
