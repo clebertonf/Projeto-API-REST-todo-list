3ab1cda20ec1475a8680a8107f489b808eb47fd2
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-31 23:27:36 -0300

    Finalizando refatorção API, resta somente documentar os endpoints

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -2,15 +2,15 @@ const rescue = require('express-rescue');
 const TodoService = require('../services/TodoServices');
 
 const listTodos = async (req, resp) => {
-  const todos = await TodoService.listTodos();
-  resp.status(200).json({ todos });
+  const tasks = await TodoService.listTodos();
+  resp.status(200).json({ tasks });
 };
 
 const createTodo = rescue(async (req, resp) => {
   const { text } = req.body;
 
-  const todo = await TodoService.createTodo(text);
-  resp.status(200).json({ message: 'Successfully created', todo });
+  const task = await TodoService.createTodo(text);
+  resp.status(200).json({ message: 'Successfully created', task });
 });
 
 const editTodo = rescue(async (req, resp) => {
